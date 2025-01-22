57a744c45420a7ae9cbac7e0bd20789823f01aa7
Kajsa Classon <ispyspiders@gmail.com>
2025-01-22 14:21:33 +0100

    Kommentarer

--- wwwroot/js/navmenu.js
+++ wwwroot/js/navmenu.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// Hämta in meny-knapparna
+// Hämta in meny-knapp och meny-element
 let menuBtn = document.getElementById("menu-btn");
 let navMenuEl = document.getElementById("nav-menu");
 
@@ -8,10 +8,9 @@ let navMenuEl = document.getElementById("nav-menu");
 //eventlyssnare
 menuBtn.addEventListener('click', toggleMenu);
 
+// Göm/visa meny
 function toggleMenu() {
     menuBtn.classList.toggle("active");
     navMenuEl.classList.toggle("active");
 
 }
-
-
